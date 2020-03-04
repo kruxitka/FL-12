ba7878e31396f82edff3199623e083c49e537fe9
kruxitka <kruxitka@gmail.com>
2020-03-04 15:42:05 +0200

    updated task1

--- FL12_Webinar_HW6/task1/js/index.js
+++ FL12_Webinar_HW6/task1/js/index.js
@@ -1,15 +1,19 @@
 // $("#someId").css({"prop" : "value", "prop1" : "value1", ...});
 
 //body: add padding 20px
+$("body").css("padding", "20px")
 
 // id title: make align center, remove top margin
+$("#title").css({"text-align" : "center", "margin-top" : 0})
 
 // div after header: add double border and padding 20px
+$("header + div").css({"border-style": "double", "padding" : "20px"})
 
 // change color for all h2 and set top margin to 0
+$("h2").css({"color": "blue", "margin-top" : 0})
 
 // set font size 18px for all .list direct child
-
+$(".list").css("font-size", "18px")
 
 
 // ******************************************************
@@ -25,22 +29,26 @@
 
 // in #list-1
   // show all hidden and not cloned li
-
+$("#list-1 li:not(.cloned)").show();
   // hide empty li
+$("#list-1 li:empty").hide();
 
 // in #list-3
   // for all even li set margin-left -20px
+$("#list-3 li:even").css({"margin-left" : "-20px"});
 
   // for the first li set any different color
-
+$("#list-3 li:first").css({"color" : "green"});
   // for all li with index > 5 set color to #ccc
+$("#list-3 li:gt(5)").css("color", "#ccc")
 
 // show ul which is parent
-
+$('ul:parent').css({'border' : 'solid 1px black'});
 // for li wich has 'em' add red color
+$("li:has(em)").css("color", "red");
 
 // for li which contains text 'Buratino' set font weight to bold
-
+$("li:contains(Buratino)").css({"font-weight" : "bold"})
 
 
 
